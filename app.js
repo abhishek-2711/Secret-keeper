83d3924b18bb99be2e294683ed25e6cca04e305e
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2022-06-23 10:49:54 +0530

    level 4 authentication using bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 5;
 const { appendFileSync } = require("fs");
 
 const app = express();
@@ -39,36 +40,42 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    if (!err) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      });
 
-  //   save() method to add user to the database
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
+      //   save() method to add user to the database
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
     }
   });
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
 
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (password === foundUser.password) {
-          res.render("secrets");
-          console.log(foundUser.password);
-        } else {
-          res.send("<h1>Incorrect password</h1>");
-        }
+        bcrypt.compare(
+          req.body.password,
+          foundUser.password,
+          function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          }
+        );
       } else {
         res.send("<h1>user not found</h1>");
       }
